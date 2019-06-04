c93e2caf6d0d06e95157bf935dad135be62e116e
xuefeng <xuefeng1@doumi.com>
2019-06-04 11:59:44 +0800

    第一次提交

---
+++ server.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XueFeng
+ * Date: 2019/6/4
+ * Time: 11:53
+ */
+
+//todo 调用各种配置文件
+/**
+ * hprose调用入口文件
+ * Class Server
+ */
+class Server
+{
+    public function __construct()
+    {
+    }
+
+}
